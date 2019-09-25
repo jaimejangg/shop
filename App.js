fb9b33a5cf8d4ec33ce87effb24c9c78cb1f6a86
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-09-25 14:06:10 -0500

    primer commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ShopNavigator from './navigation/ShopNavigation';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer
+});
+
+const store = createStore(rootReducer);
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts} 
+        onFinish={() => {
+          setFontLoaded(true);
+        }} 
+      />
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <ShopNavigator />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
