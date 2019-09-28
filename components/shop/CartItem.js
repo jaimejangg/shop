83df0b6ce9ae7ac208da5235937b240714a45788
Jaime Rueda <jaime_rueda10@hotmail.com>
2019-09-27 19:00:51 -0500

    tercer commit

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { Platform } from '@unimodules/core';
+// import { Platform } from '@unimodules/core';
 
 const CartItem = props => {
     return (
--- store/reducers/orders.js
+++ store/reducers/orders.js
@@ -13,7 +13,6 @@ export default (state = initialState, action) => {
                 action.orderData.items,
                 action.orderData.amount,
                 new Date()
-                
             );
             return {
                 ...state,
